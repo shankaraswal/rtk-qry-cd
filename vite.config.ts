3306a7b0654927b1e166fc1c7c2db6cbfea4ab56
Shankar Aswal <shankaraswal@gmail.com>
2024-08-06 19:17:17 +0100

    revert last changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure this matches your Vercel config if you have one
-  },
-  base: "", // Ensure this is correctly set if your site is not at the root
 });
