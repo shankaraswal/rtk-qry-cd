52d9c21956f0545326323db01572ba2cb43a5535
Shankar Aswal <shankaraswal@gmail.com>
2024-08-06 19:12:28 +0100

    added: vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure this matches your Vercel config if you have one
+  },
+  base: "", // Ensure this is correctly set if your site is not at the root
+});
